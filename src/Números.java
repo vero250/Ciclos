e9c47e2090cdbc7130afb004f9f4b0e9c24b403d
vcl <cruzlopezveronica241@gmail.com>
2024-09-11 16:12:50 -0600

    Se realizo ejercicio Número

---
+++ src/Números.java
@@ -0,0 +1,15 @@
+
+public class Números {
+
+	public static void main(String[] args) {
+		int maxX=3;
+		int maxY=3;
+		for (int x = 1; x <= maxX; x++) {
+			for(int y = 1; y <= maxY; y ++) {
+				System.out.println(x + "" + y);
+			}//for y
+		}//for x
+		
+	}//main
+
+}// class Números
---
+++ src/Triangulo.java
@@ -0,0 +1,9 @@
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
